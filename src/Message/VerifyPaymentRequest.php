0697b0ee3522888aba45ff2b874a5834b7e9caf4
jeewan Dhakal <jeewandhakal25@gmail.com>
2020-08-13 11:02:00 +0545

    Fix for purchase verification

--- src/Message/VerifyPaymentRequest.php
+++ src/Message/VerifyPaymentRequest.php
@@ -14,6 +14,8 @@ class VerifyPaymentRequest extends AbstractRequest
      */
     protected $verifyUrl = 'epay/transrec';
 
+    protected $agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
+
     /**
      * @return string
      */
@@ -27,19 +29,35 @@ class VerifyPaymentRequest extends AbstractRequest
         ];
     }
 
+    public function userAgent()
+    {
+        $agent = $this->agent;
+
+        if (isset($_SERVER['HTTP_USER_AGENT'])) {
+            $agent = $_SERVER['HTTP_USER_AGENT'];
+        }
+
+        return $agent;
+    }
+
     /**
      * @param mixed $data
      * @return \Omnipay\Common\Message\ResponseInterface|VerifyPaymentResponse
      */
     public function sendData($data)
     {
-        $endPoint = $this->getEndpoint().'?'.http_build_query($data);
+        $endPoint = $this->getEndpoint();
+        $headers = [
+            'User-Agent' => $this->userAgent(),
+            'Accept' => "application/xml",
+            'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8',
+        ];
 
-        $httpResponse = $this->httpClient->request('GET', $endPoint);
+        $httpResponse = $this->httpClient->request('POST', $endPoint, $headers, http_build_query($data));
 
-        $data = new SimpleXMLElement($httpResponse->getBody()->getContents());
+        $data_ = new SimpleXMLElement($httpResponse->getBody()->getContents());
 
-        return $this->response = new VerifyPaymentResponse($this, $data);
+        return $this->response = new VerifyPaymentResponse($this, $data_);
     }
 
     /**
@@ -49,6 +67,6 @@ class VerifyPaymentRequest extends AbstractRequest
     {
         $endPoint = $this->getTestMode() ? $this->sandboxUrl : $this->liveUrl;
 
-        return $endPoint.$this->verifyUrl;
+        return $endPoint . $this->verifyUrl;
     }
 }
